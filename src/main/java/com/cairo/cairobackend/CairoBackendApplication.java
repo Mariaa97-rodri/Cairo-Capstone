d205b04d1e2f2d3ce0e6071ab4bcfdc3a4bd355e
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-07 14:51:49 -0400

    initialize Cairo Spring Boot Project structure

---
+++ src/main/java/com/cairo/cairobackend/CairoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cairo.cairobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CairoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CairoBackendApplication.class, args);
+    }
+
+}
