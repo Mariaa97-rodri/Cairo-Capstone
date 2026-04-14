6d2be1b92521d689716596ca7d9fc08d4c0f41c4
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 11:52:30 -0400

    feat(repo): add AiInteractionRepository

--- src/main/java/com/cairo/cairobackend/repository/AiInteractionRepository.java
+++ src/main/java/com/cairo/cairobackend/repository/AiInteractionRepository.java
@@ -1,4 +1,11 @@
 package com.cairo.cairobackend.repository;
 
-public class AiInteractionRepository {
+import com.cairo.cairobackend.entity.AiInteraction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AiInteractionRepository extends JpaRepository<AiInteraction, Long> {
+    // Used for monitoring: how many times has a user used each feature?
+    long countByUserIdAndFeature(Long userId, AiInteraction.AiFeature feature);
 }
