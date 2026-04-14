6682f6e1fc2a6a2d0c17757d1e8077aba35febca
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:07:20 -0400

    feat(dto): add request CreateProjectRequest

--- src/main/java/com/cairo/cairobackend/dto/request/CreateProjectRequest.java
+++ src/main/java/com/cairo/cairobackend/dto/request/CreateProjectRequest.java
@@ -1,4 +1,24 @@
 package com.cairo.cairobackend.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateProjectRequest {
+
+    @NotBlank(message = "Project name is required")
+    @Size(max = 150)
+    private String name;
+
+    @NotBlank(message = "Project key is required")
+    @Size(max = 10, message = "Key cannot exceed 10 characters")
+    @Pattern(regexp = "^[A-Za-z0-9]+$",
+            message = "Key must be letters and numbers only")
+    private String projectKey;
+
+    private String description;
 }
