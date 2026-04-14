22b67e67c585dcf4b880e1689aa1dc3dba6bcaa0
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:06:42 -0400

    feat(dto): add request CreateIssueRequest

--- src/main/java/com/cairo/cairobackend/dto/request/CreateIssueRequest.java
+++ src/main/java/com/cairo/cairobackend/dto/request/CreateIssueRequest.java
@@ -1,4 +1,27 @@
 package com.cairo.cairobackend.dto.request;
 
+import com.cairo.cairobackend.entity.Issue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateIssueRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    private String description;
+
+    @NotNull(message = "Type is required")
+    private Issue.IssueType type;
+
+    @NotNull(message = "Priority is required")
+    private Issue.Priority priority;
+
+    private Long assigneeId;
+    private Long sprintId;
+    private Integer storyPoints;
 }
