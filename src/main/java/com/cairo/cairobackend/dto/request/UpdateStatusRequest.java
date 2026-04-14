4f6f85f9682062313253929a9ca84c2434bf0bd6
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:11:42 -0400

    feat(dto): add request UpdateStatusRequest

--- src/main/java/com/cairo/cairobackend/dto/request/UpdateStatusRequest.java
+++ src/main/java/com/cairo/cairobackend/dto/request/UpdateStatusRequest.java
@@ -1,4 +1,14 @@
 package com.cairo.cairobackend.dto.request;
 
+import com.cairo.cairobackend.entity.Issue;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateStatusRequest {
+
+    @NotNull(message = "Status is required")
+    private Issue.IssueStatus status;
 }
