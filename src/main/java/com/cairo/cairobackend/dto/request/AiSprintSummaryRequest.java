65e51de43f1d7d55837c987bd274f3ce88f5dddf
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:24:28 -0400

    feat(dto): add request AiSprintSummaryRequest

--- src/main/java/com/cairo/cairobackend/dto/request/AiSprintSummaryRequest.java
+++ src/main/java/com/cairo/cairobackend/dto/request/AiSprintSummaryRequest.java
@@ -1,4 +1,12 @@
 package com.cairo.cairobackend.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class AiSprintSummaryRequest {
+
+    @NotNull(message = "Sprint ID is required")
+    private Long sprintId;
 }
