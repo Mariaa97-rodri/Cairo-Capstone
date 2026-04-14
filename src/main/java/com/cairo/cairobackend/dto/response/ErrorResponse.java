f6791cb6fc4f4b9fbb81f1fe3733635c6e18d5f7
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 16:16:31 -0400

    feat(dto): add response ErrorResponse

--- src/main/java/com/cairo/cairobackend/dto/response/ErrorResponse.java
+++ src/main/java/com/cairo/cairobackend/dto/response/ErrorResponse.java
@@ -1,4 +1,19 @@
 package com.cairo.cairobackend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+    private LocalDateTime timestamp;
 }
