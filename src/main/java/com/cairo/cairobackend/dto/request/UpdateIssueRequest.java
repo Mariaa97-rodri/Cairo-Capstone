c382945522c3d61f6c96efe312d72f209e529084
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:11:01 -0400

    feat(dto): add request UpdateIssueRequest

--- src/main/java/com/cairo/cairobackend/dto/request/UpdateIssueRequest.java
+++ src/main/java/com/cairo/cairobackend/dto/request/UpdateIssueRequest.java
@@ -1,4 +1,18 @@
 package com.cairo.cairobackend.dto.request;
 
+import com.cairo.cairobackend.entity.Issue;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateIssueRequest {
+
+    private String title;
+    private String description;
+    private Issue.IssueType type;
+    private Issue.Priority priority;
+    private Long assigneeId;
+    private Long sprintId;
+    private Integer storyPoints;
 }
