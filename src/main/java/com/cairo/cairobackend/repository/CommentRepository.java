13ff07dc161ed36cc9fe57c41f8717dc964f7c91
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 11:52:04 -0400

    feat(repo): add CommentRepository

--- src/main/java/com/cairo/cairobackend/repository/CommentRepository.java
+++ src/main/java/com/cairo/cairobackend/repository/CommentRepository.java
@@ -1,4 +1,11 @@
 package com.cairo.cairobackend.repository;
 
-public class CommentRepository {
+import com.cairo.cairobackend.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByIssueIdOrderByCreatedAtAsc(Long issueId);
 }
