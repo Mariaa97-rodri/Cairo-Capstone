5297710d51b8cca5d2a409bd0a16756c4c3047cf
Mariaa97-rodri <maria.aguilar@peopleshores.com>
2026-04-14 15:14:54 -0400

    feat(dto): add response AuthResponse

--- src/main/java/com/cairo/cairobackend/dto/response/AuthResponse.java
+++ src/main/java/com/cairo/cairobackend/dto/response/AuthResponse.java
@@ -1,4 +1,19 @@
 package com.cairo.cairobackend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String message;
+    private Long userId;
+    private String name;
+    private String email;
+    private String role;
 }
